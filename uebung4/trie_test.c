34c521a414d7699e029f10e029b201bd49e2a619
TyRoXx <tyroxxxx@gmail.com>
2012-11-23 19:25:27 +0100

    uebung 4: Vorgegebenes Programmgeruest

---
+++ uebung4/trie_test.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+/* constructs a initial trie node */
+struct trie* constructTrie();
+
+/* inserts all comma seperated words into the trie */
+void insertWords(struct trie*, char*);
+
+/* searches a single word in the trie
+   return 1 if the word is found, 0 otherwise
+ */
+int searchWord(struct trie*, char*);
+
+char* inputString = 
+"takeoffs,rifling,subroutine,plead,deployments,seats,clothing,certificate,works,helmet,governors,states,"
+"tabulations,manifests,algorithm,explanations,son,progress,outline,chin,summary,junction,food,curvatures,"
+"privates,broadcast,fund,curtains,capes,digits,laundry,centers,gaskets,compliance,cameras,machinery,"
+"surfaces,screwdriver,others,provision,surprises,helmsman,attorney,foot,swabs,inceptions,bowl,tailors,"
+"quarterdecks,positions,sailor,retrieval,doorknob,perforators,breaks,traps,effectiveness,notice,budget,"
+"warning,profit,acts,setups,secrets,segments,professions,curvature,limp,rings,lens,parcels,turnarounds,"
+"superstructures,debts,thyristor,burn,maples,linkage,illustration,specification,lapses,designations,till,"
+"allegations,nulls,platter,inclination,lessons,signals,amounts,mark,passbook,buildings,sips,military,"
+"profiles,conventions,object,array,nomenclatures,diameter,features,boats,equations,base,permits,story,"
+"accountability,missiles,bands,mustard,teeth,disks,site,weathers,washes,model,dependents,gangways,voice,"
+"integers,reproduction,recombinations,badges,noun,mathematics,operators,morphine,skip,seal,terms,cruises,"
+"plexiglass,drunkeness,head,slash,byte,laughs,gravel,shadows,door,gland,aptitudes,wrap,instance,cushions,"
+"misfits,managers,closure,junk,sentry,closures,chairwoman,fake,september,change,fuses,cups,journals,cuff,"
+"round,lockers,piston,stretchers,remainders,abilities,kiloliters,rocket,summaries,sessions,grinders,sponsor,"
+"section,crewmembers,pay,jackets,meeting,hats,representative,wastes,diode,hitch,correlation,wholesales,"
+"ideals,prop,cords,mug,swell,commands,displacements,glow,keywords,bail,helms,centimeters,releases,beacons,"
+"indicator,ohm,delimiters,bore,handler,punishments,college,moisture,pleasure,subtotal,boxcars,skirt,freezes,"
+"business,types,laundries,dependence,leak,evidence,blocks,dynamics,existence,desertions,thread,billet,"
+"abuses,pace,knee,summers,equipment,draft,spike,dimensions,tries,evaluation,skills,gears,ignitions,sod,"
+"accommodation,clubs,contrasts,repair,leads,mineral,breast,moneys,west,seasoning,alignment,harm,completion,"
+"enemy,drunk,ammonia,augmentation,rose,hygiene,radiators,veteran,spacer,ammunition,adherence,answer,"
+"complement,reservoirs,jig,architecture,bricks,abrasive,powders,semicolons,coals,frigate,span,plating,"
+"blindfolds,fires,carburetors,feet,components,accidents,recipient,lifts,problems,goggles,ears,spindle,"
+"troubleshooters,term,cylinders,meals,paygrade,buy,silences,south,filler,stress,bunch,neutron,cheeses,"
+"hunte,hunde,hundehuette,hundehaus,huntehaus";
+
+int numPositivTests = 28;
+char* positivTest[] = {
+"takeoffs","tabulations","privates","broadcast","surfaces","screwdriver",
+"quarterdecks","positions","warning","profit","superstructures","debts","thyristor","allegations",
+"profiles","conventions","object","array","nomenclatures","diameter","features","boats","equations",
+"base","permits","story","hunte","huntehaus"};
+
+int numNegativTests = 28;
+char* negativTest[] = {
+"takeoff","tabullations","private","broadcst","surface","screwriver",
+"quarterdeckss","position","warnings","proit","sperstructures","dets","thristor","alegations",
+"profile","convention","objects","arrayy","nomenclatres","diameer","fetures","bots","equtions",
+"yoda","permit","storryy","hunt","hundehuet"};
+
+int main() {
+	struct trie* t = constructTrie();
+	insertWords(t, inputString);
+	int i;
+	for(i = 0; i < numPositivTests; i++) {
+		if(searchWord(t, positivTest[i]) == 0) {
+			printf("word \"%s\" was not found\n", positivTest[i]);
+		}
+	}
+	for(i = 0; i < numNegativTests; i++) {
+		if(searchWord(t, negativTest[i]) == 1) {
+			printf("word \"%s\" was incorrectly found\n", negativTest[i]);
+		}
+	}
+	return 0;
+}
