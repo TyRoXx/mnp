b4e7ff6bcbbf7125cebbecaca4f85305a7063d08
TyRoXx <tyroxxxx@gmail.com>
2012-12-07 22:23:41 +0100

    uebung 6: plausibles Ergebnis

--- uebung6/main.c
+++ uebung6/main.c
@@ -1,9 +1,55 @@
 #include <stdio.h>
-#include <stddef.h>
 #include <assert.h>
+
+typedef unsigned uint;
+
+static uint sum_of_dividers(uint dividend)
+{
+	uint sum = 1;
+	uint i;
+	uint const n = (dividend / 2);
+	for (i = 2; i <= n; ++i)
+	{
+		if (dividend % i == 0)
+		{
+			sum += i;
+		}
+	}
+	return sum;
+}
+
+static int has_friend_in_range(uint candidate, uint min, uint max)
+{
+	uint const candidate_sum = sum_of_dividers(candidate);
+	return (candidate_sum >= min) &&
+			(candidate_sum <= max) &&
+			(candidate_sum != candidate) &&
+			(candidate == sum_of_dividers(candidate_sum));
+}
+
+static uint sum_of_friends(uint min, uint max)
+{
+	uint sum = 0;
+	uint i;
+	for (i = min; i <= max; ++i)
+	{
+		if (has_friend_in_range(i, min, max))
+		{
+			sum += i;
+		}
+	}
+	return sum;
+}
 
 int main(void)
 {
+	assert(sum_of_dividers(220) == 284);
+	assert(sum_of_dividers(284) == 220);
+	assert(has_friend_in_range(220, 284, 284));
+	assert(has_friend_in_range(284, 220, 220));
+	assert(!has_friend_in_range(220, 284 + 1, 284 + 1));
+	assert(!has_friend_in_range(284, 220 + 1, 220 + 1));
+
+	printf("%u\n", sum_of_friends(0, 10000));
 	return 0;
 }
-
