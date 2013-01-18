22a3ee1acf6381c62b4ed77efb2720ebbea79aca
TyRoXx <tyroxxxx@gmail.com>
2013-01-18 20:10:21 +0100

    Uebung 8.3: Scheint zu funktionieren

--- uebung8/3/3.asm
+++ uebung8/3/3.asm
@@ -1,4 +1,12 @@
 SECTION .data
+a dd 1
+b dd 1
+c dd 1
+d dd 1
+e dd 1
+f dd 2
+g dd 0
+h dd 1
 
 SECTION .bss
 
@@ -6,12 +14,35 @@ SECTION .text
 
 global _start
 _start:
-
-
-fail:
-	mov eax, 1
-	mov ebx, 1
-	int 80h
+	; c push
+	push DWORD[c]
+	; a b *
+	mov eax, [a]
+	mul DWORD[b]
+	; pop -
+	pop ebx
+	sub eax, ebx
+	; push d push
+	push eax
+	push DWORD[d]
+	; e f %
+	mov eax, [e]
+	xor edx, edx
+	div DWORD[f]
+	mov eax, edx
+	; g -
+	sub eax, [g]
+	; pop /
+	pop ebx
+	xchg eax, ebx
+	xor edx, edx
+	div ebx
+	; h /
+	xor edx, edx
+	div DWORD[h]
+	; pop +
+	pop ebx
+	add eax, ebx
 
 succeed:
 	mov eax, 1
